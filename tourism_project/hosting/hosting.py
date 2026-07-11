889f5be44621ea18d7fa79c2fa63d5aafde205d7
vishal3007 <vishal3007@gmail.com>
2026-07-11 14:40:49 +0530

    Update hosting.py

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
-    repo_id="agarvish/Tourism_Project_Prediction",          # the target repo
+    repo_id="agarvish/Tourism-Project-Prediction",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
